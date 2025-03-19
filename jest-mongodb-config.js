1e9a1370eef81ae187a146a7f542ce494da69370
John Bower <john@industrialassets.co.uk>
2025-03-19 14:59:15 +0000

    try changing mongodb config to get CI checks working

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -3,11 +3,14 @@ export default {
     binary: {
       skipMD5: true
     },
-    autoStart: false,
     instance: {
-      dbName: 'farming-grants-agreements-api'
+      dbName: 'farming-grants-agreements-api',
+      replSet: {
+        count: 1,
+        storageEngine: 'wiredTiger'
+      }
     }
   },
   mongoURLEnvName: 'MONGO_URI',
-  useSharedDBForAllJestWorkers: false
+  useSharedDBForAllJestWorkers: true
 }
