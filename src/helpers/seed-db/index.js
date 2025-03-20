71029dbc17a655c70f3f0f7799d58b8d184581f5
John Bower <john@industrialassets.co.uk>
2025-03-20 11:23:29 +0000

    update

--- src/helpers/seed-db/index.js
+++ src/helpers/seed-db/index.js
@@ -12,29 +12,31 @@ if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test') {
 } else {
   await mongoose
     .connect(`${config.get('mongoUri')}${config.get('mongoDatabase')}`)
+    .then(async () => {
+      logger.info(`Mongoose connected`)
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      for (const [name, model] of Object.entries(models)) {
+        try {
+          await model.db.dropCollection(model.collection.name)
+          logger.info(`Dropped collection '${model.collection.name}'`)
+
+          await model.insertMany(data[model.collection.name])
+          logger.info(
+            `Successfully inserted ${data[model.collection.name].length} documents into the '${model.collection.name}' collection`
+          )
+        } catch (error) {
+          logger.error(error)
+          throw error
+        }
+      }
+
+      await mongoose.disconnect()
+      logger.info(`Mongoose disconnected`)
+      return null
+    })
     .catch((error) => {
       logger.error(`Error connecting to MongoDB: ${error.message}`)
-      throw error
+      return null
     })
-
-  logger.info(`Mongoose connected`)
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  for (const [name, model] of Object.entries(models)) {
-    try {
-      await model.db.dropCollection(model.collection.name)
-      logger.info(`Dropped collection '${model.collection.name}'`)
-
-      await model.insertMany(data[model.collection.name])
-      logger.info(
-        `Successfully inserted ${data[model.collection.name].length} documents into the '${model.collection.name}' collection`
-      )
-    } catch (error) {
-      logger.error(error)
-      throw error
-    }
-  }
-
-  await mongoose.disconnect()
-  logger.info(`Mongoose disconnected`)
 }
