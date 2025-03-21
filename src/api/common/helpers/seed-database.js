571b3f68ba9e197ba18b5a81369adbc9a31fa416
John Bower <john@industrialassets.co.uk>
2025-03-21 11:03:01 +0000

    wait for connection to database before seeding

--- src/api/common/helpers/seed-database.js
+++ src/api/common/helpers/seed-database.js
@@ -1,7 +1,13 @@
+import mongoose from 'mongoose'
 import models from '~/src/api/common/models/index.js'
 import data from '~/src/api/common/helpers/sample-data/index.js'
 
 export async function seedDatabase(logger) {
+  while (mongoose.connection.readyState !== mongoose.STATES.connected) {
+    logger.info('Waiting for mongoose to connect')
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+  }
+
   for (const [name, model] of Object.entries(models)) {
     try {
       await model.db.dropCollection(name)
