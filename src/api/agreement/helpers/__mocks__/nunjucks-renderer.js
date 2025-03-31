87f0ac3e86577effedd5788e9bc684bd7b0abdda
John Bower <john@industrialassets.co.uk>
2025-03-31 14:23:35 +0100

    tidy up

--- src/api/agreement/helpers/__mocks__/nunjucks-renderer.js
+++ src/api/agreement/helpers/__mocks__/nunjucks-renderer.js
@@ -1,6 +1,6 @@
-const renderTemplate = jest.fn((templatePath, data) => {
-  // Mock rendering logic
-  return `<html><body>Test HTML with ${data.agreementNumber}</body></html>`
-})
+const renderTemplate = jest.fn(
+  (templatePath, data) =>
+    `<html><body>Test HTML with ${data.agreementNumber}</body></html>`
+)
 
 export { renderTemplate }
--- src/api/common/models/__mocks__/agreements.js
+++
@@ -1,6 +0,0 @@
-import mockAgreementData from './mock-agreement-data.js'
-
-export default {
-  updateOne: jest.fn(),
-  findOne: jest.fn().mockResolvedValue(mockAgreementData)
-}
