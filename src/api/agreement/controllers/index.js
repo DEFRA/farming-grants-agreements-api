1ff9745bd2fd766628e0791b10a1a25a78fcbbba
John Bower <john@industrialassets.co.uk>
2025-04-25 15:42:49 +0100

    creates unaccept agreement endpoint and added more sample data

--- src/api/agreement/controllers/index.js
+++ src/api/agreement/controllers/index.js
@@ -1,9 +1,11 @@
 import { getHTMLAgreementDocumentController } from '~/src/api/agreement/controllers/get-html-agreement-document.js'
 import { acceptAgreementDocumentController } from '~/src/api/agreement/controllers/accept-agreement-document.js'
+import { unacceptAgreementDocumentController } from '~/src/api/agreement/controllers/unaccept-agreement-document.js'
 import { viewAgreementDocumentController } from '~/src/api/agreement/controllers/view-agreement-document.js'
 
 export {
   getHTMLAgreementDocumentController,
   acceptAgreementDocumentController,
+  unacceptAgreementDocumentController,
   viewAgreementDocumentController
 }
