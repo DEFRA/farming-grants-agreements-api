6df84a78e44800f5f6ca5834979cd242bb7f4cbb
Bill Sclater (DEFRA) <bill.sclater@equalexperts.com>
2025-07-03 14:09:35 +0100

    added extra agreement for testing and ids to tables

--- src/api/common/helpers/sample-data/agreements.js
+++ src/api/common/helpers/sample-data/agreements.js
@@ -573,5 +573,319 @@ export default [
         totalAgreementPayment: 12617.59
       }
     }
+  },
+  {
+    agreementNumber: 'SFI88888888',
+    agreementName: 'Sample Agreement',
+    correlationId: uuidv4(),
+    frn: '8888888888',
+    sbi: '888888888',
+    company: 'Sample Farm Ltd',
+    address: '123 Farm Lane, Farmville',
+    postcode: 'FA12 3RM',
+    username: 'John Doe',
+    agreementStartDate: startDate,
+    agreementEndDate: endDate,
+    actions: [
+      {
+        code: 'BND1',
+        title: 'Maintain dry stone walls',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'BND2',
+        title: 'Maintain earth banks or stone-faced hedgebanks',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CHRW1',
+        title: 'Assess and record hedgerow condition',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CHRW2',
+        title: 'Manage hedgerows',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CHRW3',
+        title: 'Maintain or establish hedgerow trees',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CLIG3',
+        title:
+          'Manage grassland with very low nutrient inputs - outside SDAs or within SDAs',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CSAM1',
+        title:
+          'Assess soil, test soil organic matter and produce a soil management plan',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'CSAM1A',
+        title:
+          'Assess soil, produce a soil management plan and test soil organic matter',
+        startDate,
+        endDate,
+        duration: '3 years'
+      },
+      {
+        code: 'GRH8',
+        title: 'Supplement: Haymaking (late cut)',
+        startDate,
+        endDate,
+        duration: '3 years'
+      }
+    ],
+    parcels: [
+      {
+        parcelNumber: 'SX64592661',
+        parcelName: '',
+        totalArea: 9.7091,
+        activities: [
+          {
+            code: 'BND1',
+            description: 'Maintain dry stone walls',
+            area: 95.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'BND2',
+            description: earthBanksDescription,
+            area: 1678.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CHRW1',
+            description: assessHedgerowDescription,
+            area: 1799.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CHRW2',
+            description: manageHedgerowDescription,
+            area: 1799.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CHRW3',
+            description: manageHedgerowTreesDescription,
+            area: 717.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CLIG3',
+            description: manageGrasslandDescription,
+            area: 8.64,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CSAM1',
+            description:
+              'Assess soil, test soil organic matter and produce a soil management plan',
+            area: 10.63,
+            startDate,
+            endDate
+          },
+          {
+            code: 'CSAM1A',
+            description: assessSoilDescription,
+            area: 1.0,
+            startDate,
+            endDate
+          },
+          {
+            code: 'GRH8',
+            description: 'Supplement: Haymaking (late cut)',
+            area: 9.7091,
+            startDate,
+            endDate
+          }
+        ]
+      }
+    ],
+    payments: {
+      activities: [
+        {
+          code: 'BND1',
+          description: 'Maintain dry stone walls',
+          quantity: 95.0,
+          rate: 25.65,
+          measurement: '95.00 Metres',
+          paymentRate: '£25.65/100m',
+          annualPayment: 25.65
+        },
+        {
+          code: 'BND2',
+          description: earthBanksDescription,
+          quantity: 1678.0,
+          rate: 11.0,
+          measurement: '1678.00 Metres',
+          paymentRate: '11.00/100m',
+          annualPayment: 184.58
+        },
+        {
+          code: 'CHRW1',
+          description: assessHedgerowDescription,
+          quantity: 1799.0,
+          rate: 5.0,
+          measurement: '1799.00 Metres',
+          paymentRate: '5.00/100m',
+          annualPayment: 89.95
+        },
+        {
+          code: 'CHRW2',
+          description: manageHedgerowDescription,
+          quantity: 1799.0,
+          rate: 13.0,
+          measurement: '1799.00 Metres',
+          paymentRate: '13.00/100m',
+          annualPayment: 233.87
+        },
+        {
+          code: 'CHRW3',
+          description: manageHedgerowTreesDescription,
+          quantity: 717.0,
+          rate: 10.0,
+          measurement: '717.00 Metres',
+          paymentRate: '10.00/100m',
+          annualPayment: 71.7
+        },
+        {
+          code: 'CLIG3',
+          description: manageGrasslandDescription,
+          quantity: 8.64,
+          rate: 151.0,
+          measurement: '8.64 HA',
+          paymentRate: '151.00/ha',
+          annualPayment: 1304.56
+        },
+        {
+          code: 'CSAM1',
+          description:
+            'Assess soil, test soil organic matter and produce a soil management plan',
+          quantity: 10.63,
+          rate: 6.0,
+          measurement: '10.63 HA',
+          paymentRate: '6.00/ha',
+          annualPayment: 63.78
+        },
+        {
+          code: 'CSAM1A',
+          description: assessSoilDescription,
+          quantity: 1.0,
+          rate: 97.0,
+          measurement: '1.00 Units',
+          paymentRate: '97.00/units',
+          annualPayment: 97.0
+        },
+        {
+          code: 'GRH8',
+          description: 'Supplement: Haymaking (late cut)',
+          quantity: 9.71,
+          rate: 187.0,
+          measurement: '9.71 HA',
+          paymentRate: '187.00/ha',
+          annualPayment: 1815.61
+        }
+      ],
+      totalAnnualPayment: 3886.69,
+      yearlyBreakdown: {
+        details: [
+          {
+            code: 'BND1',
+            year1: 25.65,
+            year2: 25.65,
+            year3: 25.65,
+            totalPayment: 76.95
+          },
+          {
+            code: 'BND2',
+            year1: 184.58,
+            year2: 184.58,
+            year3: 184.58,
+            totalPayment: 553.74
+          },
+          {
+            code: 'CHRW1',
+            year1: 89.95,
+            year2: 89.95,
+            year3: 89.95,
+            totalPayment: 269.85
+          },
+          {
+            code: 'CHRW2',
+            year1: 233.87,
+            year2: 233.87,
+            year3: 233.87,
+            totalPayment: 701.61
+          },
+          {
+            code: 'CHRW3',
+            year1: 71.7,
+            year2: 71.7,
+            year3: 71.7,
+            totalPayment: 215.1
+          },
+          {
+            code: 'CLIG3',
+            year1: 1304.56,
+            year2: 1304.56,
+            year3: 1304.56,
+            totalPayment: 3913.68
+          },
+          {
+            code: 'CSAM1',
+            year1: 63.78,
+            year2: 63.78,
+            year3: 63.78,
+            totalPayment: 191.31
+          },
+          {
+            code: 'CSAM1A',
+            year1: 97.0,
+            year2: 97.0,
+            year3: 97.0,
+            totalPayment: 291.0
+          },
+          {
+            code: 'GRH8',
+            year1: 1815.61,
+            year2: 1815.61,
+            year3: 1815.61,
+            totalPayment: 5446.83
+          }
+        ],
+        annualTotals: {
+          year1: 3886.69,
+          year2: 3886.69,
+          year3: 3886.69
+        },
+        totalAgreementPayment: 11660.07
+      }
+    }
   }
 ]
