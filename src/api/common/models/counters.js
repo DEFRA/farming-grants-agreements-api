4173483de9a952f72803c9e122494a9390252773
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2025-07-31 09:39:15 +0100

    Use a single source of truth for database schema, JSDoc and validation

--- src/api/common/models/counters.js
+++ src/api/common/models/counters.js
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
+import { z } from 'zod'
+import { zodSchema as zodMongooseSchema } from '@zodyac/zod-mongoose'
 
 const collection = 'counters'
 
-const schema = new mongoose.Schema(
-  {
-    _id: { type: String, required: true },
-    seq: { type: Number, required: true }
-  },
-  {
-    collection,
-    timestamps: false
-  }
-)
+const counterSchema = z.object({
+  _id: z.string(),
+  seq: z.number()
+})
+
+const schema = zodMongooseSchema(counterSchema, {
+  collection,
+  timestamps: false
+})
 
 export default mongoose.model(collection, schema)
