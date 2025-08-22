6d1274500a5948d226950422de2cdf04cadc5d8c
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2025-08-22 19:04:54 +0100

    Refactor publishAgreementEvents to not nest more than 3 if/for/while/switch/try statements.

--- src/api/common/helpers/seed-database.js
+++ src/api/common/helpers/seed-database.js
@@ -3,6 +3,24 @@ import models from '~/src/api/common/models/index.js'
 import sampleData from '~/src/api/common/helpers/sample-data/index.js'
 import { publishEvent } from '~/src/api/common/helpers/sns-publisher.js'
 
+async function publishAgreementEvents(tableData, logger) {
+  for (const row of tableData) {
+    await publishEvent(
+      {
+        topicArn:
+          'arn:aws:sns:eu-west-2:000000000000:grant_application_approved',
+        type: 'io.onsite.agreement.application.approved',
+        time: new Date().toISOString(),
+        data: row
+      },
+      logger
+    )
+  }
+  logger.info(
+    `Successfully inserted ${tableData.length} documents into the 'agreements' collection`
+  )
+}
+
 export async function seedDatabase(logger) {
   while (mongoose.connection.readyState !== mongoose.STATES.connected) {
     logger.info('Waiting for mongoose to connect...')
@@ -21,22 +39,7 @@ export async function seedDatabase(logger) {
 
       const tableData = sampleData[name]
       if (tableData?.length && name === 'agreements') {
-        for (const row of tableData) {
-          await publishEvent(
-            {
-              topicArn:
-                'arn:aws:sns:eu-west-2:000000000000:grant_application_approved',
-              type: 'io.onsite.agreement.application.approved',
-              time: new Date().toISOString(),
-              data: row
-            },
-            logger
-          )
-        }
-
-        logger.info(
-          `Successfully inserted ${tableData.length} documents into the '${name}' collection`
-        )
+        await publishAgreementEvents(tableData, logger)
       }
     } catch (e) {
       logger.error(e)
