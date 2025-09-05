c2f71470968b2923987f2fe4b957d7d4b63a37a8
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2025-09-05 09:11:04 +0100

    SFIR-107: Show/hide of print pdf button and  Working version of printable agreement doc when js working

--- src/client/javascripts/application.js
+++ src/client/javascripts/application.js
@@ -1,16 +1,15 @@
-import {
-  createAll,
-  Button,
-  Checkboxes,
-  ErrorSummary,
-  Header,
-  Radios,
-  SkipLink
-} from 'govuk-frontend'
+import { initAll } from 'govuk-frontend'
 
-createAll(Button)
-createAll(Checkboxes)
-createAll(ErrorSummary)
-createAll(Header)
-createAll(Radios)
-createAll(SkipLink)
+initAll()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.gem-c-print-link__button')
+  if (!btn) {
+    return
+  }
+  btn.type = 'button'
+  btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    window.print()
+  })
+})
