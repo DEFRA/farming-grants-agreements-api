83a8d493405aa37df5010c50ffacd284ab650dcf
Bill Sclater <charliejade8888@gmail.com>
2025-10-23 12:11:10 +0100

    SFIR-552: Remove Nunjucks templates and static file serving (#189)

--- src/config/nunjucks/filters/filters.test.js
+++
@@ -1,8 +0,0 @@
-import * as filters from './filters.js'
-
-describe('nunjucks filters export', () => {
-  test('should export formatDate and formatCurrency functions', () => {
-    expect(typeof filters.formatDate).toBe('function')
-    expect(typeof filters.formatCurrency).toBe('function')
-  })
-})
