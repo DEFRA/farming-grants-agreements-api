e60f56ef862ed2e7e064f2a05ec63361965629fb
Bill Sclater (DEFRA) <bill.sclater@equalexperts.com>
2025-11-13 10:01:13 +0000

    added loggin to help diagnose create-offer message mapping issue

--- src/api/common/helpers/sqs-message-processor/create-agreement.js
+++ src/api/common/helpers/sqs-message-processor/create-agreement.js
@@ -14,6 +14,24 @@ export const handleCreateAgreementEvent = async (
 ) => {
   if (payload?.type?.includes('gas-backend.agreement.create')) {
     logger.info(`Creating agreement from event: ${notificationMessageId}`)
+    logger.info(
+      `Incoming message payload structure: ${JSON.stringify({
+        hasData: !!payload.data,
+        hasAnswers: !!payload.data?.answers,
+        hasAnswersPayment: !!payload.data?.answers?.payment,
+        hasAnswersApplicant: !!payload.data?.answers?.applicant,
+        hasApplication: !!payload.data?.application,
+        hasApplicationApplicant: !!payload.data?.application?.applicant,
+        clientRef: payload.data?.clientRef,
+        code: payload.data?.code,
+        identifiers: payload.data?.identifiers
+      })}`
+    )
+    if (logger?.debug) {
+      logger.debug(
+        `Full incoming message data: ${JSON.stringify(payload.data, null, 2)}`
+      )
+    }
     const agreement = await createOffer(
       notificationMessageId,
       payload.data,
