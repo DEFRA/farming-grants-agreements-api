c72678d6052672d75cb9efbbee0887e8f762f843
SeedSyntax <221134143+SeedSyntax@users.noreply.github.com>
2025-11-18 15:26:26 +0000

    Update to the latest active Node LTS version

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,17 +1,15 @@
-export default {
-  mongodbMemoryServerOptions: {
-    binary: {
-      skipMD5: true
-    },
-    autoStart: false,
-    instance: {
-      dbName: 'farming-grants-agreements-api'
-    },
-    replSet: {
-      count: 3,
-      storageEngine: 'wiredTiger'
-    }
+export const mongodbMemoryServerOptions = {
+  binary: {
+    skipMD5: true
   },
-  mongoURLEnvName: 'MONGO_URI',
-  useSharedDBForAllJestWorkers: false
+  autoStart: false,
+  instance: {
+    dbName: 'farming-grants-agreements-api'
+  },
+  replSet: {
+    count: 3,
+    storageEngine: 'wiredTiger'
+  }
 }
+export const mongoURLEnvName = 'MONGO_URI'
+export const useSharedDBForAllJestWorkers = false
