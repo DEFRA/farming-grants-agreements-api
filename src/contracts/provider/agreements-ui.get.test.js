3d85382faf171e0f4450fc9393eb8475231144a6
Seb <221134143+SeedSyntax@users.noreply.github.com>
2025-12-09 09:25:28 +0000

    Self-hosted runner passes in PACT_USER/PASS (#250)

--- src/contracts/provider/agreements-ui.get.test.js
+++ src/contracts/provider/agreements-ui.get.test.js
@@ -77,8 +77,8 @@ describe('UI sending a GET request to get an agreement', () => {
               process.env.PACT_BROKER_URL ??
               'https://ffc-pact-broker.azure.defra.cloud',
             consumerVersionSelectors: [{ latest: true }],
-            pactBrokerUsername: process.env.PACT_BROKER_USERNAME,
-            pactBrokerPassword: process.env.PACT_BROKER_PASSWORD,
+            pactBrokerUsername: process.env.PACT_USER,
+            pactBrokerPassword: process.env.PACT_PASS,
             publishVerificationResult:
               process.env.PACT_PUBLISH_VERIFICATION === 'true',
             providerVersion: process.env.SERVICE_VERSION ?? '1.0.0'
