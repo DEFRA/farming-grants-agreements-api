7d13541fc2acf1e63ab4397f1364ba2dbd77148e
asifshaik04 <45974468+asifshaik04@users.noreply.github.com>
2026-03-04 16:44:07 +0000

    SFIR-1101: First cut version of sending grant_payments event to GPS

--- src/api/agreement/controllers/accept-offer.controller.js
+++ src/api/agreement/controllers/accept-offer.controller.js
@@ -4,6 +4,45 @@ import { unacceptOffer } from '#~/api/agreement/helpers/unaccept-offer.js'
 import { updatePaymentHub } from '#~/api/agreement/helpers/update-payment-hub.js'
 import { config } from '#~/config/index.js'
 import { publishEvent } from '#~/api/common/helpers/sns-publisher.js'
+import { sendMessage } from '~/src/api/common/helpers/sqs-send-message.js'
+
+const grantPaymentPayload = {
+  sbi: '106284736',
+  frn: '12544567',
+  claimId: 'R00000004',
+  grants: [
+    {
+      sourceSystem: 'FPTT',
+      paymentRequestNumber: 1,
+      correlationId: '7cf9bd11-c791-42c9-bd28-fa0fecb2d92c',
+      invoiceNumber: 'R00000004-V001Q2',
+      originalInvoiceNumber: 'R00000004-V001Q1',
+      agreementNumber: 'FPTT264870631',
+      totalAmount: '702.85',
+      currency: 'GBP',
+      marketingYear: '2026',
+      accountCode: 'SOS710',
+      fundCode: 'DRD10',
+      payments: [
+        {
+          dueDate: '2026-06-05',
+          totalAmount: '12.63',
+          status: 'pending',
+          invoiceLines: [
+            {
+              schemeCode: 'CMOR1',
+              description: '2026-06-05: Parcel 8083',
+              amount: '12.63'
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+const queueUrl =
+  'http://localhost:4566/000000000000/gps__sqs__create_payment.fifo'
 
 /**
  * Controller to accept agreement offer
@@ -34,6 +73,20 @@ const acceptOfferController = async (request, h) => {
       await unacceptOffer(agreementNumber)
       throw err
     }
+
+    request.logger?.info?.(`********* Before sending CREATE_GRANT_PAYMENT`)
+
+    // sending grant payment request
+    await sendMessage(
+      {
+        queueUrl,
+        type: 'CREATE_GRANT_PAYMENT',
+        data: grantPaymentPayload
+      },
+      request.logger
+    )
+
+    request.logger?.info?.(`********* After sending CREATE_GRANT_PAYMENT`)
 
     // Publish event to SNS
     await publishEvent(
