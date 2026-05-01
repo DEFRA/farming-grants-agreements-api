5be35a04c1f9a766df505367c93c74e9bf278214
Asif Shaik <45974468+asifshaik04@users.noreply.github.com>
2026-05-01 10:46:57 +0100

    FGP-1025: Clearing grants on seed DB true (#400)

--- src/api/common/models/index.js
+++ src/api/common/models/index.js
@@ -1,10 +1,12 @@
 import agreements from './agreements.js'
+import grants from './grant.js'
 import versions from './versions.js'
 import invoices from './invoices.js'
 import counters from './counters.js'
 
 export default {
   agreements,
+  grants,
   versions,
   invoices,
   counters
